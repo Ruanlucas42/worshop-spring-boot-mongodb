9df006593c9304c5fa71d803b1f90a06d804d784
Ruan Lucas <rulucasilva13@gmail.com>
2025-01-25 13:50:29 -0300

    projeto criado

---
+++ src/test/java/com/ruanlucas/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruanlucas.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
