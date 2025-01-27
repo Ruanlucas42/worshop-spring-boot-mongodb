eece1987bd925919fdb22fc0f25239f5dd5c1b29
Ruan Lucas <rulucasilva13@gmail.com>
2025-01-27 14:42:49 -0300

    obtendo Post por id

---
+++ src/main/java/com/ruanlucas/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.ruanlucas.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ruanlucas.workshopmongo.domain.Post;
+import com.ruanlucas.workshopmongo.respository.PostRepository;
+import com.ruanlucas.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+			return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado")); 
+	}
+}
